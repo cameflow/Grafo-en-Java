d51c41477bc0162a676796be11371e650117c2e1
Alejandro <alex.terrazas.z@gmail.com>
2015-11-01 22:32:18 -0600

    :rocket: Creation of class node

--- node.java
+++ node.java
@@ -1,1 +1,39 @@
-//Clase Nodo
+
+public class node{
+   int id;
+   boolean visited = false;
+   String data;
+
+
+   void setId(int a)
+   {
+     id = a;
+   }
+   void setVisited()
+   {
+     if (visited)
+     {
+       visited = false;
+     }
+     else
+     {
+       visited = true;
+     }
+   }
+   void setData(String dat)
+   {
+     data = dat;
+   }
+   int getId()
+   {
+     return id;
+   }
+   boolean getVisited()
+   {
+     return visited;
+   }
+   String returnData()
+   {
+     return data;
+   }
+}
